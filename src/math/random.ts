ed64ed6ba4cfa7f0412e68dd69192189a5eb90d8
luddebudde <109613465+luddebudde@users.noreply.github.com>
2026-06-14 16:55:47 +0000

    Second attempt to redeploy

--- src/math/random.ts
+++ src/math/random.ts
@@ -1,9 +1,10 @@
 
-import { vectorFromAngle } from './angle.ts'
+import { vectorFromAngle } from './angle'
+import { Vec2 } from './vec'
 
 export const random = (min: number, max: number) => {
   return Math.random() * (max - min) + min
 }
 
-export const randomUnitVector = (): Vector =>
+export const randomUnitVector = (): Vec2 =>
   vectorFromAngle(Math.random() * 2 * Math.PI)
